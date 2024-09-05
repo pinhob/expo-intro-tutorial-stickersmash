eb3e766c2c1a8313e572885a23325f15aeddd8db
pinhob <20286747+pinhob@users.noreply.github.com>
2024-09-05 00:04:23 -0300

    feat: continue tutorial

--- App.js
+++ App.js
@@ -4,18 +4,62 @@ import { StyleSheet, View} from "react-native";
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import CircleButton from "./components/CircleButton";
+import IconButton from "./components/IconButton";
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(true);
+
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert ('You did not select any image.')
+    }
+  }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  }
+
+  const onAddSticker = () => {}
+
+  const onSaveImageAsync = async () => {}
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
-        <Button label="Use this photo" />
+
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
       </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -34,5 +78,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,13 @@
 import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
 import { View, Pressable, Text, StyleSheet } from "react-native";
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
+
   if (theme === "primary") {
     return (
       <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
-        <Pressable style={[styles.button, { backgroundColor: "#fff" }]} onPress={() => alert('You pressed a button.')}>
+        <Pressable style={[styles.button, { backgroundColor: "#fff" }]} onPress={onPress}>
           <FontAwesome
             name="picture-o"
             size={18}
@@ -20,7 +22,7 @@ export default function Button({ label, theme }) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
