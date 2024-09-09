2fd6b8f8330e0f2a7db957e3a4a85acc7f018034
pinhob <20286747+pinhob@users.noreply.github.com>
2024-09-09 18:36:06 -0300

    feat: implements onSaveImageAsync for web

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -7,8 +7,9 @@ import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CircleButton from "./components/CircleButton";
 import IconButton from "./components/IconButton";
 import EmojiPicker from "./components/EmojiPicker";
@@ -54,6 +55,7 @@ export default function App() {
   }
 
   const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -67,6 +69,23 @@ export default function App() {
     } catch (error) {
       console.log(error);
     }
+  } else {
+    try {
+      const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+        quality: 0.95,
+        width: 320,
+        height: 440,
+      })
+  
+      let link = document.createElement('a');
+      link.download = 'sticker-smash.jpeg';
+      link.href = dataUrl;
+      link.click();
+    } catch (error) {
+      
+    }
+  }
+
   }
   const onModalClose = () => {
     setIsModalVisible(false);
